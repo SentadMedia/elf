ef2b6365257bddc919a3ac7f69de797259443bc8
Soufiane Mghanen <mgh.soufiane@gmail.com>
2020-01-29 12:26:19 +0000

    added new types

--- modern/mdgraphql/graphql.go
+++ modern/mdgraphql/graphql.go
@@ -1,10 +1,12 @@
 package mdgraphql
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/graph-gophers/graphql-go"
-
+	"github.com/graph-gophers/graphql-go/log"
 	"github.com/graph-gophers/graphql-go/relay"
 	"github.com/sentadmedia/elf/fw"
 	"github.com/sentadmedia/elf/modern/mdhttp"
@@ -26,10 +28,10 @@ func (g GraphGophers) ListenAndServe(port int) error {
 }
 
 func NewGraphGophers(graphqlPath string, logger fw.Logger, tracer fw.Tracer, g fw.GraphQLAPI) fw.Server {
-	relayHandler := NewRelayHandler(g)
+	relayHandler := NewRelayHandler(g, logger)
 
 	server := mdhttp.NewServer(logger, tracer)
-	server.HandleFunc(graphqlPath, relayHandler)
+	server.HandleFunc(graphqlPath, middlewareOne(relayHandler))
 
 	return GraphGophers{
 		logger: logger,
@@ -47,13 +49,33 @@ func (r RelayHandler) ServeHTTP(writer http.ResponseWriter, reader *http.Request
 	r.handler.ServeHTTP(writer, reader)
 }
 
-func NewRelayHandler(g fw.GraphQLAPI) RelayHandler {
+func NewRelayHandler(g fw.GraphQLAPI, logger fw.Logger) RelayHandler {
 	schema := graphql.MustParseSchema(
 		g.GetSchema(),
 		g.GetResolver(),
 		graphql.UseStringDescriptions(),
+		graphql.Logger(logger.(log.Logger)),
 	)
-	return RelayHandler{handler: relay.Handler{
-		Schema: schema,
-	}}
+	return RelayHandler{
+		handler: relay.Handler{
+			Schema: schema,
+		},
+	}
+}
+
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var params struct {
+			Query         string                 `json:"query"`
+			OperationName string                 `json:"operationName"`
+			Variables     map[string]interface{} `json:"variables"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		fmt.Print(fmt.Sprintf("params=%+v", params))
+		next.ServeHTTP(w, r)
+	})
 }
