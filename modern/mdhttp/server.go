2f273f8b296a086cd6a67fd1c65b5cd79941c717
Soufiane Mghanen <mgh.soufiane@gmail.com>
2020-02-07 17:38:30 +0000

    added new types

--- modern/mdhttp/server.go
+++ modern/mdhttp/server.go
@@ -46,7 +46,7 @@ func (s Server) HandleFunc(pattern string, handler http.Handler) {
 }
 
 func setupPreFlight(w http.ResponseWriter) http.ResponseWriter {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers",
 		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
